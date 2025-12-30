b5ba413a18166679172b0e5aec818a1299aa509e
Pratik Potdukhe <63575398+pratikpotdukhe30@users.noreply.github.com>
2025-12-30 12:23:33 +0100

    String methods in java

---
+++ StringMethods.java
@@ -0,0 +1,8 @@
+char ch = text.charAt(i);
+      if (vowels.contains(String.valueOf(ch))) {
+
+// to pick a character from a particular index
+char ch = string.charAt(index)
+
+// to check if substring is present in the main string  
+string.contains(substring)  
