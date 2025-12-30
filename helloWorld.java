9d0c2bfa101b65711f5474d5facf0661b68ece52
Pratik Potdukhe <63575398+pratikpotdukhe30@users.noreply.github.com>
2025-12-30 11:33:38 +0100

    I printed another statement with two main methods
    Added a new Main class with an additional main method that prints a different message.

--- helloWorld.java
+++ helloWorld.java
@@ -3,3 +3,12 @@ public class Main {
     System.out.println("Hello World");
   }
 }
+
+
+// This is another approach
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World Shravani");
+  }
+}
